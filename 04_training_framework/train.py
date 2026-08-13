2a48a6c6b1c1901809838063685d1d8c3caf14fe
qiusensei <171793892@qq.com>
2026-08-14 01:20:23 +0800

    feat: add seed in cfg and use it

--- 04_training_framework/train.py
+++ 04_training_framework/train.py
@@ -1,9 +1,12 @@
+import torch
 import models.cnn as cnn
 import configs.default as cfg
 from datasets.mnist import get_loaders
 from utils.train import train_loop
 from utils.checkpoint import save_model
 from utils.plot import plot_img
+
+torch.manual_seed(cfg.seed)
 
 net = cnn.CNN().to(cfg.device)
 train_loader,test_loader = get_loaders(cfg.DATA_DIR, cfg.train_batch_size, cfg.test_batch_size)
