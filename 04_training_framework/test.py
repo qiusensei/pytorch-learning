4c3a6fb263af884a4cef1fd2ea67509bf8745641
qiusensei <171793892@qq.com>
2026-08-15 12:25:32 +0800

    fix: fix test.py with get_loaders()

--- 04_training_framework/test.py
+++ 04_training_framework/test.py
@@ -1,11 +1,16 @@
 import torch
 import models.cnn as cnn
 import datasets.mnist as mnist
+import configs.default as cfg
 from utils.evaluate import evaluate
 from utils.checkpoint import load_model
+
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 net = cnn.CNN().to(device)
 net = load_model(net)
-acc = evaluate(net, mnist.test_loader, device)
+loader = mnist.get_loaders(cfg.DATA_DIR,cfg.train_batch_size,cfg.test_batch_size)
+test_loader = loader[1]
+acc = evaluate(net, test_loader, device)
+
 print(f"Test acc: {acc:.4f}")
